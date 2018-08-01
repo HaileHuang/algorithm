c0f894e1e52cdef3443f8d959efe6dab260ba97b
Haile <Haile_ma@126.com>
2018-08-01 17:06:07 +0800

    Update Qsort.js

--- Qsort.js
+++ Qsort.js
@@ -15,3 +15,30 @@ function Qsort(a, l, r) {
   Qsort(a, l, i - 1);
   Qsort(a, i + 1, r);
 }
+
+//Qsort with partition
+function partition(a, l, r) {
+  if (l >= r) return 
+  var i = l, j = r, key = a[l]
+  while(i<j) {
+    while(i<j&&a[j] >= key) {
+      j--
+    }
+    a[i] = a[j]
+    while(i<j&&a[i] <= key){
+      i++
+    }
+    a[j] = a[i]
+  }
+  a[i] = key
+  return i
+}
+function qsort(a, l, r){
+  var index = partition(a, l, r)
+  if (index > l) {
+    qsort(a, l, index - 1)
+  }
+  if (index < r) {
+    qsort(a, index+1, r)
+  }
+}
