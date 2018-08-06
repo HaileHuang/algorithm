a88fce3b85c6a9d6ccf848ff934b4bb7641aba72
Haile <Haile_ma@126.com>
2018-08-06 10:47:54 +0800

    Update Qsort.js

--- Qsort.js
+++ Qsort.js
@@ -42,3 +42,36 @@ function qsort(a, l, r){
     qsort(a, index+1, r)
   }
 }
+
+// merge_sorrt
+function merge(a, tmp_a, l, r, mid){
+  var i = l, j= mid + 1, k = l;
+  while(i <= mid && j <= r) {
+    if (a[i] < a[j]){
+      tmp_a[k] = a[i];
+      i++;
+    } else {
+      tmp_a[k] = a[j]
+      j++;
+    }
+    k++;
+  }
+  while(i <= mid){
+    tmp_a[k] = a[i];
+    i++; k++;
+  }
+  while(j <= r){
+    tmp_a[k] = a[j];
+    j++; k++;
+  }
+  console.log(l,r,mid,tmp_a)
+  for(i = l; i <= r; i++) a[i] = tmp_a[i]
+}
+
+function merge_sort(a, tmp_a, l, r) {
+  if (l >= r) return ;
+  var mid = parseInt((l + r)/2)
+  merge_sort(a, tmp_a, l, mid)
+  merge_sort(a, tmp_a, mid+1,r)
+  merge(a,tmp_a,l,r,mid)
+}
